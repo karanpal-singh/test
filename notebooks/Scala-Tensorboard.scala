2f3da7483da8427d82c0ff393798473af8006181
Karan <palsingh_k@yahoo.com>
2017-05-22 14:29:18 +0530

    worked testing

---
+++ notebooks/Scala-Tensorboard.scala
@@ -0,0 +1,15 @@
+// Databricks notebook source
+// MAGIC %sh netstat -ntlp | grep LISTEN
+
+// COMMAND ----------
+
+// MAGIC %sh 
+// MAGIC 
+// MAGIC curl localhost:6006
+
+// COMMAND ----------
+
+// MAGIC %sh lsof -i :6006
+
+// COMMAND ----------
+
